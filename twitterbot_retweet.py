9eb3000a59152b08b96cd3b66357e255b2a068d1
Gavin Greer <gavin@sineanalytics.com>
2018-05-10 23:55:54 -0500

    added time parameters to retweet

--- twitterbot_retweet.py
+++ twitterbot_retweet.py
@@ -1,14 +1,22 @@
 import tweepy
 from time import sleep
 from credentials import *
+from datetime import datetime,timedelta
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+
+
 def retweet(q, interval):
+	t = datetime.now()
+	today = t.strftime('%Y-%m-%d')
+	lw = datetime.now() - timedelta(days=5)
+	lastwk = lw.strftime('%Y-%m-%d')
+
 	print('start')
-	for tweet in tweepy.Cursor(api.search,q=q,lang='en').items(10):
+	for tweet in tweepy.Cursor(api.search,q=q,since=lastwk,until=today,lang='en').items(10):
 		try:
 			print('\nTweet by: @' + tweet.user.screen_name)
 			tweet.retweet()
