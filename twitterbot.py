3be42f397d4d17955388411cb7ded0cd9cae2ee4
Gavin Greer <gavin@sineanalytics.com>
2018-05-15 11:09:44 -0500

    consolidate bot scripts

--- twitterbot.py
+++ twitterbot.py
@@ -7,16 +7,14 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-
-
-def retweet(q, interval):
+def retweet(q, interval, loops):
 	t = datetime.now()
 	today = t.strftime('%Y-%m-%d')
 	lw = datetime.now() - timedelta(days=5)
 	lastwk = lw.strftime('%Y-%m-%d')
 
 	print('start')
-	for tweet in tweepy.Cursor(api.search,q=q,since=lastwk,until=today,lang='en').items(10):
+	for tweet in tweepy.Cursor(api.search,q=q,since=lastwk,until=today,lang='en').items(loops):
 		try:
 			print('\nTweet by: @' + tweet.user.screen_name)
 			tweet.retweet()
@@ -35,4 +33,23 @@ def retweet(q, interval):
 
 		except StopIteration:
 			break
+
+def tweet(filename):
+	my_file = open(filename, 'r')
+	file_lines = my_file.readlines()
+	my_file.close()
+	for line in file_lines:
+		try:
+			print(line)
+			if line != '\n':
+				api.update_status(line)
+				sleep(15)
+			else: 
+				pass
+	
+		except tweepy.TweepError as e:
+			print(e.reason)
+			sleep(2)
+
+
 
